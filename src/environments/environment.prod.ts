9de6360249200f2d828dbdf002b62cdd2db2b679
rsperoni <rsperoni@mgcoders.com>
2018-05-06 13:30:07 -0300

    endpoint prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://sulfur.magnesium.coop/api',
+  apiUrl: 'https://sulfur.magnesium.link/api',
   hmr: false,
   showLoggs: false,
   showIdColumns: false,
