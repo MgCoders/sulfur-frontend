5a259af273944ec23328576068528dd65b8b64d2
cristianbauza <cristian.bauza.silva@gmail.com>
2019-06-20 00:11:12 -0300

    Parametrizando la url del icono.

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 
 find . -type f -name '*.js' -exec sed -i "s|backend-endpoint|${BACKEND_URL}|" {} +
 
+find . -type f -name '*.html' -exec sed -i "s|ico-url-show|${ICO_URL}|" {} +
+
 nginx -g 'daemon off;'
